1f9e60ef8c0ebe512f50bbb177359c860a4afc5d
Filip Chrapek <hello@filipchrapek.com>
2023-10-05 19:33:09 +0200

    Add OG images

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,7 @@
 import { slugifyStr } from "@utils/slugify";
 import Datetime from "./Datetime";
 import type { CollectionEntry } from "astro:content";
+import { Image } from "astro:assets";
 
 export interface Props {
   href?: string;
@@ -9,27 +10,36 @@ export interface Props {
 }
 
 export default function Card({ href, frontmatter, secHeading = true }: Props) {
-  const { title, pubDatetime, description } = frontmatter;
+  const { title, pubDatetime, description, ogImage } = frontmatter;
 
   const headerProps = {
     style: { viewTransitionName: slugifyStr(title) },
-    className: "text-xl font-bold tracking-tighter decoration-dashed group-hover:underline",
+    className: "line-clamp-2 text-xl mb-2 font-bold tracking-tighter decoration-dashed group-hover:underline",
   };
 
   return (
     <li className="my-6 sm:my-14 sm:last-of-type:my-5">
       <a
         href={href}
-        className="group mb-1 inline-block decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        className="group grid grid-cols-6 gap-6 decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
-        {secHeading ? (
-          <h2 {...headerProps}>{title}</h2>
-        ) : (
-          <h3 {...headerProps}>{title}</h3>
-        )}
-
-        <Datetime className="mb-4" datetime={pubDatetime} />
-        <p className="text-skin-secondary">{description}</p>
+        <div className="col-span-4" >
+          {secHeading ? (
+            <h2 {...headerProps}>{title}</h2>
+          ) : (
+            <h3 {...headerProps}>{title}</h3>
+          )}
+          <p className="display-none sm:webkit-box mb-4 line-clamp-2 text-skin-secondary">{description}</p>
+          <Datetime datetime={pubDatetime} />
+        </div>
+          
+        <img
+          src={ogImage}
+          width="400"
+          height="300"
+          alt={title}
+          className="col-span-2"
+        />  
       </a>
     </li>
   );
